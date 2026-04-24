29f981db04091b3dc1b8ab1ecc65a9369c8e0c5d
jonahliew8881 <jonahliew8881@gmail.com>
2026-04-25 02:43:07 +0800

    Update accounts.js

---
+++ frontend/src/constants/accounts.js
@@ -0,0 +1,13 @@
+export const ACCOUNTS = [
+  { id: 'citi',  label: 'Citi',  currency: 'HKD', capital: 187320 },
+  { id: 'yiu',   label: '耀才', currency: 'HKD', capital: 160000 },
+  { id: 'futu',  label: '富途', currency: 'HKD', capital: 120000 },
+  { id: 'ibkr',  label: 'IBKR',  currency: 'USD', capital: 180000 },
+]
+
+export const ACCOUNT_COLORS = {
+  citi:  '#ffd700',
+  yiu:   '#ff6b35',
+  futu:  '#a855f7',
+  ibkr:  '#00d4ff',
+}
