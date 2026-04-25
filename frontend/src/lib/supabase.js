f77e96d3070a98bb5e0f8ca867369bb007d8e6f7
jonahliew8881 <jonahliew8881@gmail.com>
2026-04-26 01:58:25 +0800

    fix: revert to env variables

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://hcmbuqxuopxybfrnuugy.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjbWJ1cXh1b3B4eWJmcm51dWd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNDU5MzIsImV4cCI6MjA5MjYyMTkzMn0.nxRY2cbuY5PzkLI0cf2gCbJOlnvv4QjG_AuqKrYX13k'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
